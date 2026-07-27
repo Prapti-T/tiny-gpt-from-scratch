6245bc19478c7f73f6ce9e358d954d1542917dbf
Prapti-T <85100556+Prapti-T@users.noreply.github.com>
2026-07-27 11:09:18 +0545

    Step 13: index_element

--- model.py
+++ model.py
@@ -105,8 +105,11 @@ def make_2d_random(rows, cols, seed):
     res = np.random.rand(rows, cols)
     return res
 
-# Step 13 - index_element (not yet solved)
-# TODO: implement
+# Step 13 - index_element
+def index_element(arr, i, j):
+    """Return the scalar element at position (i, j) of a 2D array."""
+    # TODO: return the value at row i, column j of arr
+    return arr[i][j]
 
 # Step 14 - slice_row (not yet solved)
 # TODO: implement
