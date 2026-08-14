b211823be095e2254d70e0fd7133e62f2840fec5
Prapti-T <85100556+Prapti-T@users.noreply.github.com>
2026-08-14 16:06:50 +0545

    Step 60: forward_logits_onehot

--- model.py
+++ model.py
@@ -511,8 +511,10 @@ def one_hot_encode_batch(ids, vocab_size):
         res[i][t] = 1.0
     return res
 
-# Step 60 - forward_logits_onehot (not yet solved)
-# TODO: implement
+# Step 60 - forward_logits_onehot
+def forward_logits_onehot(onehot, w_matrix):
+    # TODO: compute logits for the neural bigram model as the matrix product of one-hot inputs and W.
+    return matmul(onehot, w_matrix)
 
 # Step 61 - observe_lookup_equivalence (not yet solved)
 # TODO: implement
